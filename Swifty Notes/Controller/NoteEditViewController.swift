2594c63fd4a47ba742c64892b063d0edf1824154
ko kyaw <kyaw.codes@gmail.com>
2021-05-02 18:39:09 +0630

    Fix textView

--- Swifty Notes/Controller/NoteEditViewController.swift
+++ Swifty Notes/Controller/NoteEditViewController.swift
@@ -20,8 +20,10 @@ class NoteEditViewController: UIViewController {
     private var dataSource = NoteDataSource.shared
     
     private let textView = UITextView(font: .systemFont(ofSize: 18))
-    private let scrollView = UIScrollView()
-    private var scrollViewBottomAnchor: NSLayoutConstraint?
+    private var textViewHeightConstraint: NSLayoutConstraint!
+    
+    private let topPadding: CGFloat = 12
+    private let sidePadding: CGFloat = 20
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +36,14 @@ class NoteEditViewController: UIViewController {
         setupNavigationBar()
         
         textView.delegate = self
-        setupScrollView()
+        textView.showsVerticalScrollIndicator = false
+        view.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: sidePadding).isActive = true
+        textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topPadding).isActive = true
+        textView.widthAnchor.constraint(equalToConstant: view.frame.width - 2 * sidePadding).isActive = true
+        textViewHeightConstraint = textView.heightAnchor.constraint(equalToConstant: view.frame.height - 2 * topPadding)
+        textViewHeightConstraint?.isActive = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -48,42 +57,15 @@ class NoteEditViewController: UIViewController {
             guard let self = self else { return }
             guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
             let keyboardHeight = value.cgRectValue.height
-            self.scrollViewBottomAnchor!.constant = -keyboardHeight + self.view.safeAreaInsets.bottom
+            self.textViewHeightConstraint!.constant = self.view.frame.height - (self.view.safeAreaInsets.top + self.view.safeAreaInsets.bottom + keyboardHeight)
         }
         
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: nil) { [weak self] (notification) in
             guard let self = self else { return }
-            self.scrollViewBottomAnchor!.constant = 0
+            self.textViewHeightConstraint!.constant = self.view.frame.height - (2 * self.topPadding + self.view.safeAreaInsets.top)
         }
     }
-    
-    fileprivate func setupTextView() {
-        scrollView.addSubview(textView)
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-        textView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-        textView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        textView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-        textView.widthAnchor.constraint(equalToConstant: view.frame.width - 32).isActive = true
-        
-        let heightConstraint = textView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
-        heightConstraint.priority = .defaultLow
-        heightConstraint.isActive = true
-    }
-    
-    fileprivate func setupScrollView() {
-        view.addSubview(scrollView)
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
-        scrollViewBottomAnchor = scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        scrollViewBottomAnchor?.isActive = true
-        
-        setupTextView()
-    }
-    
+
     private func setupNavigationBar() {
         if note == nil {
             title = "New Note"
